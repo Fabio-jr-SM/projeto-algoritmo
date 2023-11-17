9c2b94ae4c5f5c407767819bf5d59a1adf6fa87d
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-11-17 10:45:41 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
+//https://www.cprogressivo.net/2013/11/Como-ler-arquivos-em-C-As-funcoes-fgetc-fscanf-fgets.html
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <unistd.h>
-
-#define INFINITY (1<<20);
+#define INFINITY (1<<20)
 
 typedef struct p
 {
@@ -13,8 +12,6 @@ typedef struct p
     char dataNascimento[11];
 } PESSOA;
 
-void menu();
-void criarArquivo();
 void carregaArquivo();
 void imprimeArquivo();
 void editaArquivo();
@@ -27,54 +24,32 @@ PESSOA lista[100];
 int cont;
 FILE *arquivo;
 
-//Criar arquivo e escrever nele no formato %d ; %s ; %s ; %s
-void criarEscreverArquivo(){
-    arquivo = fopen("pessoa.txt", "w+");
-    int i;
-    //Laço de repetição responsável por percorrer o vetor
-    for(i = 0; i<cont; i++)
-    {
-        //Escreve no arquivoNovo.txt seguindo um novo formato
-        fprintf(arquivo, "%d ; %s ; %s ; %s\n", lista[i].IDPessoa, lista[i].nome, lista[i].cpf, lista[i].dataNascimento);
-    }
-    //Fecha os dois arquivos que foram abertos com a função fopen
-    fclose(arquivo);
-}
-
 void carregaArquivo()
 {
+    
     cont = 0;
     arquivo = fopen("pessoa.txt", "r+");
-    while(fscanf(arquivo,"%d ; %s ; %s ; %s", &lista[cont].IDPessoa, lista[cont].nome, lista[cont].cpf, lista[cont].dataNascimento)!= EOF)
-    {
-        cont++;
-    }
-    fclose(arquivo);
-}
-
-//1 - CADASTRAR
-void cadastraArquivo()
-{
-    int op;
-    printf("1 - Cadastrar Pessoa\n2 - Cadastrar Telefone\n");
-    scanf("%d",&op);
     
-    if(op==1){
-        PESSOA novaPessoa;
-        //Cadastrar o registro na memória
-        printf("Digite o nome\n");
-        scanf("%s", novaPessoa.nome);
-        printf("Digite o CPF\n");
-        scanf("%s", novaPessoa.cpf);
-        printf("Digite a data de nascimento\n");
-        scanf("%s", novaPessoa.dataNascimento);
-        
-        criarEscreverArquivo();
-    } 
+    if (arquivo != NULL) {
+        while(fscanf(arquivo,"%d ; %s ; %s ; %s", &lista[cont].IDPessoa, lista[cont].nome, lista[cont].cpf, lista[cont].dataNascimento)!= EOF){
+            cont++;
+        }
+        fclose(arquivo);
+    }else{
+        arquivo = fopen("pessoa.txt", "w+");
+        fclose(arquivo);
+    }
 }
 
+void imprimeArquivo()
+{
+    int i;
+    for(i = 0; i<cont; i++)
+    {
+        printf("%d ; %s ; %s ; %s\n", lista[i].IDPessoa, lista[i].nome, lista[i].cpf, lista[i].dataNascimento);
+    }
+}
 
-//2 - EDITAR
 void editaArquivo()
 {
     //Alterar o registro na memória
@@ -94,20 +69,15 @@ void editaArquivo()
     }
     
     arquivo = fopen("pessoa.txt", "w+");
-    //Laço de repetição responsável por percorrer o vetor
     for(i = 0; i<cont; i++)
     {
-        //Escreve no arquivoNovo.txt seguindo um novo formato
         fprintf(arquivo, "%d ; %s ; %s ; %s\n", lista[i].IDPessoa, lista[i].nome, lista[i].cpf, lista[i].dataNascimento);
     }
-    //Fecha os dois arquivos que foram abertos com a função fopen
     fclose(arquivo);
 }
 
-//3 - EXCLUIR
 void excluiArquivo()
 {
-    //Excluir o registro na memória
     arquivo = fopen("pessoa.txt", "w+");
     int IDPessoaExclusao;
     printf("Digite o ID para exclusao\n");
@@ -123,14 +93,20 @@ void excluiArquivo()
     fclose(arquivo);
 }
 
-//4 - CONSULTAR
-void imprimeArquivo()
+void cadastraArquivo()
 {
-    int i;
-    for(i = 0; i<cont; i++)
-    {
-        printf("%d ; %s ; %s ; %s\n", lista[i].IDPessoa, lista[i].nome, lista[i].cpf, lista[i].dataNascimento);
-    }
+    PESSOA novaPessoa;
+    printf("Digite o nome\n");
+    scanf("%s", novaPessoa.nome);
+    printf("Digite o CPF\n");
+    scanf("%s", novaPessoa.cpf);
+    printf("Digite a data de nascimento\n");
+    scanf("%s", novaPessoa.dataNascimento);
+    
+    arquivo = fopen("pessoa.txt", "a+");
+    fprintf(arquivo, "%d ; %s ; %s ; %s\n", ultimoID()+1, novaPessoa.nome, novaPessoa.cpf, novaPessoa.dataNascimento);
+    fclose(arquivo);
+    
 }
 
 int ultimoID()
@@ -146,11 +122,11 @@ int ultimoID()
     }
 }
 
-//VOID MENU
 void menu()
 {
     int menu;
     do{
+        printf("\n============CADASTRO TELEFONICO============\n");
         printf("1 - Cadastrar\n");
         printf("2 - Editar\n");
         printf("3 - Excluir\n");
@@ -189,5 +165,6 @@ void menu()
 int main()
 {
     menu();
+
     return 0;
 }
