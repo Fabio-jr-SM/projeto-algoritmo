c39efac5e9e6dd51b6106fb36971ae015a1603e2
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-12-01 10:45:16 -0400

    Update agenda.h

--- agenda.h
+++ agenda.h
@@ -1,4 +1,15 @@
-// agenda.h
+/**
+ * O arquivo de cabeçalho "agenda.h" contém a definição de tipos, constantes e protótipos de funções relacionados ao programa de cadastro telefônico.
+ * Define padrões de expressões regulares para validação de dados como email, telefone e data.
+ * Também define a estrutura PESSOA, que representa as informações de uma pessoa no cadastro.
+ *
+ * @note Inclui a biblioteca "stdbool.h" para utilizar o tipo de dado booleano.
+ * @note Define macros para expressões regulares de email, telefone e data.
+ * @note Declaração da estrutura PESSOA, que armazena informações de uma pessoa no cadastro.
+ * @note Protótipos de funções para as operações principais do programa.
+ * @note Certifique-se de incluir este arquivo em outros módulos do programa para acessar as definições e funcionalidades.
+ */
+
 #ifndef AGENDA_H
 #define AGENDA_H
 
@@ -8,16 +19,18 @@
 #define TELEFONE "\\([0-9]{2}\\)[0-9]{4,5}-[0-9]{4}"
 #define DATA_PATTERN "\\d{2}/\\d{2}/\\d{4}"
 
+// Definição da estrutura PESSOA
 typedef struct p {
     int IDPessoa;
     char nome[31];
     char email[31];
     char cpf[12];
     char dataNascimento[11];
-    char **telefones;
+    char **telefones; 
     int numTelefones;
 } PESSOA;
 
+// Protótipos de funções
 void menu();
 void carregaArquivo();
 void escreverArquivo();
