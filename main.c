91981063e57a44fd1aa107732a3388fec998b737
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-12-01 10:44:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-// main.c
+/**
+ * O programa principal inicia a execução chamando a função menu() do módulo "agenda.h".
+ *
+ * @note Inclui o cabeçalho "agenda.h" para ter acesso às definições e funcionalidades do programa.
+ * @note Chama a função menu() que é responsável por iniciar a interação do usuário com o sistema.
+ * @note Retorna 0 para indicar que a execução do programa foi concluída sem erros.
+ */
+
 #include "agenda.h"
 
 int main()
 {
-    menu();
-    return 0;
+    menu();  // Chama a função menu() para iniciar a interação com o usuário.
+    return 0;  // Retorna 0 para indicar que a execução foi concluída sem erros.
 }
